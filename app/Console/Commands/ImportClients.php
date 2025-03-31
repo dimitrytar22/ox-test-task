da61b1353ab1d5c541e96efa02cb2004f17e6a0a
dimitrytar22 <dtarasenko212006@gmail.com>
2025-03-31 06:21:16 +0300

    moved import to ClientService

--- app/Console/Commands/ImportClients.php
+++ app/Console/Commands/ImportClients.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Http\Services\ClientsAPIService;
+use App\Http\Services\ClientService;
 use App\Models\Client;
 use App\Models\Item;
 use App\Models\Order;
@@ -30,48 +31,6 @@ class ImportClients extends Command
      */
     public function handle()
     {
-        $clients = ClientsAPIService::getClients();
-        foreach ($clients as $client) {
-            $orders = $client['orders'];
-            $createdClient = Client::query()->createOrFirst([
-                'email' => $client['email'],
-                'phone' => $client['phone']
-            ], [
-                'full_name' => $client['full_name'],
-                'email' => $client['email'],
-                'phone' => $client['phone'],
-                'address' => $client['address'],
-                'date_of_birth' => $client['date_of_birth']
-            ]);
-            foreach ($orders as $order) {
-                $items = $order['items'];
-                $createdStatus = Status::query()->firstOrCreate([
-                    'title' => $order['status'],
-                ],[
-                    'title' => $order['status']
-                ]);
-                $createdOrder = Order::query()->createOrFirst([
-                    'client_id' => $createdClient->id,
-                    'status_id' => $createdStatus->id
-                ], ['client_id' => $createdClient->id,
-                    'status_id' => $createdStatus->id
-                ]);
-                foreach ($items as $item) {
-                    $createdItem = Item::query()->createOrFirst([
-                        'title' => $item['title'],
-                        'price' => $item['price']
-                    ], [
-                        'title' => $item['title'],
-                        'price' => $item['price']
-                    ]);
-                    $itemQuantity = $item['quantity'];
-                    $dataToAttach = [
-                        $createdItem->id => ['quantity' => $itemQuantity]
-                    ];
-                    $createdOrder->items()->attach($dataToAttach);
-                }
-
-            }
-        }
+        echo ClientService::importClients() ? "Imported successfully" : "Import Error";
     }
 }
--- app/Http/Services/ClientService.php
+++ app/Http/Services/ClientService.php
@@ -5,6 +5,10 @@ namespace App\Http\Services;
 use App\Http\Requests\Client\StoreRequest;
 use App\Http\Requests\Client\UpdateRequest;
 use App\Models\Client;
+use App\Models\Item;
+use App\Models\Order;
+use App\Models\Status;
+use Mockery\Exception;
 
 class ClientService
 {
@@ -22,18 +26,72 @@ class ClientService
         return Client::query()->firstOrCreate([
             'email' => $data['email'],
             'phone' => $data['phone'],
-        ],$data);
+        ], $data);
 
     }
 
     public function destroy(Client $client)
     {
         $orders = $client->orders;
-        foreach ($orders as $order){
+        foreach ($orders as $order) {
             $order->items()->detach();
             $order->delete();
         }
         $client->delete();
+    }
+
+    public static function importClients()
+    {
+
+        try {
+            $clients = ClientsAPIService::getClients();
+
+            foreach ($clients as $client) {
+                $orders = $client['orders'];
+                $createdClient = Client::query()->createOrFirst([
+                    'email' => $client['email'],
+                    'phone' => $client['phone']
+                ], [
+                    'full_name' => $client['full_name'],
+                    'email' => $client['email'],
+                    'phone' => $client['phone'],
+                    'address' => $client['address'],
+                    'date_of_birth' => $client['date_of_birth']
+                ]);
+                foreach ($orders as $order) {
+                    $items = $order['items'];
+                    $createdStatus = Status::query()->firstOrCreate([
+                        'title' => $order['status'],
+                    ], [
+                        'title' => $order['status']
+                    ]);
+                    $createdOrder = Order::query()->createOrFirst([
+                        'client_id' => $createdClient->id,
+                        'status_id' => $createdStatus->id
+                    ], ['client_id' => $createdClient->id,
+                        'status_id' => $createdStatus->id
+                    ]);
+                    foreach ($items as $item) {
+                        $createdItem = Item::query()->firstOrCreate([
+                            'title' => $item['title'],
+                            'price' => $item['price']
+                        ], [
+                            'title' => $item['title'],
+                            'price' => $item['price']
+                        ]);
+                        $itemQuantity = $item['quantity'];
+                        $dataToAttach = [
+                            $createdItem->id => ['quantity' => $itemQuantity]
+                        ];
+                        $createdOrder->items()->attach($dataToAttach);
+                    }
+
+                }
+            }
+        } catch (\Exception $e) {
+            return false;
+        }
+        return true;
     }
 
 }
